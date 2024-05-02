97f25047cdcb27e8871428eb9d9f68e6e8dae5fc
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-05-02 14:15:46 -0600

    added an array of questions for user input

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+// Array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
